8115be5b49576ad2b2cfa6a3cd79815036341fab
Drew-E-Wilson <dwilsrock21@gmail.com>
2021-06-10 23:23:59 -0400

    updated footer with links

--- src/Components/footer/Footer.js
+++ src/Components/footer/Footer.js
@@ -1,10 +1,17 @@
 import styles from './Footer.module.css';
+import { AiOutlineCopyright, AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 
 export default function Footer() {
 
     return (
         <div className={styles.footer_container}>
-            <h2>Drew Wilson - 2021</h2>
+            <div className={styles.sep_container}>
+                <div className={styles.footer_sites_container}>
+                    <a href="https://www.linkedin.com/in/drew-e-wilson/" target="_blank"><h2 className={styles.icon_logos}><AiFillLinkedin /></h2></a>
+                    <a href="https://github.com/Drew-E-Wilson" target="_blank"><h2 className={styles.icon_logos}><AiFillGithub /></h2></a>
+                </div>
+                <h4>Copyright <AiOutlineCopyright /> Drew Wilson ~ 2021</h4>
+            </div>
         </div>
     )
 }
